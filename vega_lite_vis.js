34f9cf12e1564c782c8e357024ea2cf16ab5185b
mzMarkZheng <mzMarkZheng@gmail.com>
2022-10-09 23:28:06 +1100

    Almost done

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -24,6 +24,6 @@ vegaEmbed("#RoadChart", vg_3).then(function (result) {
 
 
 
-vegaEmbed("#year_fat", vg_4).then(function (result) {
+vegaEmbed("#YearFat", vg_4).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
