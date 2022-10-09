364ab3bb8bd0d1c0a2285ba75c59e7e0d3ca52b0
mzMarkZheng <mzMarkZheng@gmail.com>
2022-10-09 18:41:32 +1100

    changed data

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,11 @@
-var vg_1 = "Motor_vehicles.json";
-var vg_2 = "Population.json";
-var vg_3 = "Road_length.json";
+// Map visualisation
+var vg_1 = "Map_idiom/Motor_vehicles.json";
+var vg_2 = "Map_idiom/Population.json";
+var vg_3 = "Map_idiom/Road_length.json";
+
+
+// 
+var vg_4 = "Fatalities_bar_chart/year_fatalities.json";
 
 
 
@@ -14,5 +19,11 @@ vegaEmbed("#PopulationChart", vg_2).then(function (result) {
 }).catch(console.error);
 
 vegaEmbed("#RoadChart", vg_3).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+
+vegaEmbed("#year_fat", vg_4).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
