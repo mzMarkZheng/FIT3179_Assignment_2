5f19a90ed0e00fcf61c407c1b11690f92ba64e7a
mzMarkZheng <mzMarkZheng@gmail.com>
2022-10-17 07:50:19 +1100

    Finished all idioms

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -8,6 +8,7 @@ var vg_3 = "Map_idiom/Road_length.json";
 var vg_4 = "Fatalities_bar_chart/year_fatalities.json";
 
 
+var vg_5 = "Fatalities_heatmap/heatmap.json";
 
 
 vegaEmbed("#MotorChart", vg_1).then(function (result) {
@@ -22,8 +23,11 @@ vegaEmbed("#RoadChart", vg_3).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-
-
 vegaEmbed("#YearFat", vg_4).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+vegaEmbed("#Heatmap", vg_5).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
