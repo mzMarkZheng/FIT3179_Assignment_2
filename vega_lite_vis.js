e92bdcaf11f94e30b7732a7421d3868738e300c4
mzMarkZheng <mzMarkZheng@gmail.com>
2022-10-17 13:27:32 +1100

    Use template

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -7,8 +7,11 @@ var vg_3 = "Map_idiom/Road_length.json";
 // 
 var vg_4 = "Fatalities_bar_chart/year_fatalities.json";
 
-
 var vg_5 = "Fatalities_heatmap/heatmap.json";
+
+var vg_6 = "Map_idiom/population_pyramid.json";
+
+var vg_7 = "Map_idiom/sankiel.json";
 
 
 vegaEmbed("#MotorChart", vg_1).then(function (result) {
@@ -29,5 +32,13 @@ vegaEmbed("#YearFat", vg_4).then(function (result) {
 
 
 vegaEmbed("#Heatmap", vg_5).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#PopulationPyramid", vg_6).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#Sankiel", vg_7).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
